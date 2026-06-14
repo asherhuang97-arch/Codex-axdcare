f07b8fae015382e162dde8bc4ee0966ba1a72ddf
asherhuang97-arch <asherhuang97@gmail.com>
2026-06-14 15:47:38 +0800

    Deploy static site via Next shell

---
+++ app/layout.js
@@ -0,0 +1,13 @@
+export const metadata = {
+  title: "AXDCARE | OEM/ODM Home Medical Device Manufacturer",
+  description:
+    "AXDCARE is a home medical device OEM/ODM manufacturer for blood pressure monitors, nebulizers and thermometers."
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
